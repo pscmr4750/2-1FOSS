a479879d38d85b25a385158c9032b746e201d483
pscmr4750 <114943735+pscmr4750@users.noreply.github.com>
2022-11-09 10:11:04 +0530

    Create e5.sh

--- e5.sh
+++ e5.sh
@@ -4,7 +4,7 @@ echo "Enter a,b:"
 read a b
 echo "a=$a \t b=$b"
 echo "a)Add \n b) Subtract \n c)Multiply \n d)Divide \n e)Remainder"
-echo "\nRead option"
+echo "\nEnter option"
 read op
 case $op in
 	a) res=`expr $a + $b`
